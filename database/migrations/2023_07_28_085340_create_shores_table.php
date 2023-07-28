28515a577195c7337845e37c041c2a78cc02f6d6
Ericson-Manalo <ericmana98@gmail.com>
2023-07-28 11:27:54 +0200

    created my table in my migration

--- database/migrations/2023_07_28_085340_create_shores_table.php
+++ database/migrations/2023_07_28_085340_create_shores_table.php
@@ -15,6 +15,17 @@ return new class extends Migration
     {
         Schema::create('shores', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 60);
+            $table->string('location', 200);
+            $table->smallInteger('tot_umbrella');
+            $table->smallInteger('tot_sunbeds');
+            $table->float('price_umbrella_a_day', 3,2);
+            $table->date('opening_date');
+            $table->date('closing_date');
+            $table->boolean('beach_volley_court');
+            $table->boolean('soccer_pitch');
+            
+            
             $table->timestamps();
         });
     }
